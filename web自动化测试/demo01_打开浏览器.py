858e0dbe3305fb1c139e2acfcf5f34ab23e6aa94
wengwenyu <wengwenyu@aliyun.com>
2020-03-28 14:03:48 +0800

    使用selenium打开谷歌浏览器-打开本地网页

--- web自动化测试/demo01_打开浏览器.py
+++ web自动化测试/demo01_打开浏览器.py
@@ -2,8 +2,8 @@ import time
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.get('https://www.baidu.com')  # 网址输入必须带协议 http https
-
-time.sleep(5)
+# driver.get('https://www.baidu.com')  # 网址输入必须带协议 http https
+driver.get(r"C:\Users\wengw\Documents\automation_test\web自动化测试\素材\注册A.html")
+time.sleep(3)
 
 driver.quit()
