0c80fa2cdd71e406b1a89d491a54040a67fd845a
wengwenyu <wengwenyu@aliyun.com>
2020-04-01 17:19:34 +0800

    启动浏览器

--- Selenium3与Python3实战Web自动化测试框架/demo01_start_browser.py
+++ Selenium3与Python3实战Web自动化测试框架/demo01_start_browser.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Firefox()
-url = "http://www.baidu.com"
+# driver = webdriver.Firefox()
+driver = webdriver.Chrome()
+# driver = webdriver.Edge()
+url = "http://www.5itest.cn/register?goto=http%3A//www.5itest.cn/"
 driver.get(url)
 time.sleep(3)
 driver.quit()
